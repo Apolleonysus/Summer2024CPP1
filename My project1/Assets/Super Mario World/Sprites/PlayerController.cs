0eeff23c793669ab0e5a61433cac63926ac00d7d
Apolleonysus <geoff.p7190@gmail.com>
2024-07-23 08:59:40 -0400

    Update PlayerController.cs

--- My project1/Assets/Super Mario World/Sprites/PlayerController.cs
+++ My project1/Assets/Super Mario World/Sprites/PlayerController.cs
@@ -20,5 +20,10 @@ public class RigidbodyCollider : MonoBehaviour
         float hInput = Input.GetAxis("Horizontal");
 
         rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        }
     }
 }
