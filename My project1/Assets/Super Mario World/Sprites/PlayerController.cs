ef757461fb9095f7b5423f6efe56a62f197ed05d
Apolleonysus <geoff.p7190@gmail.com>
2024-07-23 10:04:56 -0400

    Update PlayerController.cs

--- My project1/Assets/Super Mario World/Sprites/PlayerController.cs
+++ My project1/Assets/Super Mario World/Sprites/PlayerController.cs
@@ -2,20 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
 public class RigidbodyCollider : MonoBehaviour
 {
     [SerializeField, Range(1, 20)]
     private float speed = 5;
     [SerializeField, RangeAttribute(1, 20)]
     private float jumpForce = 10;
+    [SerializeField, Range(0.01f, 1)]
+    private float groundCheckRadius = 0.02f;
+    [SerializeField] private LayerMask isGroundLayer;
 
-    private Trnasform groundCheck;
+
+    private Transform groundCheck;
+    private bool isGrounded = false;
 
     Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator anim;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
 
         if (speed <= 0)
         {
@@ -37,13 +45,19 @@ public class RigidbodyCollider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+        
         float hInput = Input.GetAxis("Horizontal");
+
 
         rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
 
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            rb.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+
+        if (hInput != 0) sr.flipX = (hInput < 0);
     }
+
 }
